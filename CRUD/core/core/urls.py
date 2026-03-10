d101580e3c5662b21fdcee1f4e27f872ed15d9da
A.Navaneetha <anavaneethasankar@gmail.com>
2026-03-10 10:28:30 +0530

    Add files via upload

---
+++ CRUD/core/core/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from recipe import views
+"""
+URL configuration for core project.
+...
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.recipes),
+    path('update_recipe/<id>', views.update_recipe, name='update_recipe'),
+   path('delete_recipe/<id>', views.delete_recipe, name='delete_recipe'),
+]
+
+# This is the part you need to add
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
